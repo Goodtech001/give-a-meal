6c2ad6ef6e23a2589b331e54602c4a3e068f61c3
goodtech001 <goodnewsnnebedim@gmail.com>
2026-03-19 14:25:23 +0100

    donate

--- app/donate/page.tsx
+++ app/donate/page.tsx
@@ -1,9 +1,8 @@
+"use client"
 import AboutDonate from '@/layout/about-donate'
 import DonateHero from '@/layout/donate-hero'
 import Footer from '@/layout/footer'
 import Subscribe from '@/layout/subscribe'
-import TopNavBar from '@/layout/Topnavbar'
-import React from 'react'
 
 function Donate() {
   return (
