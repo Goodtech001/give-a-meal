b9c7d6dd9a6c7a5761cb72830645cd79326c25dd
jehovah-son <chukwukasuccess0@gmail.com>
2026-03-25 21:23:45 +0100

    `Added modal confirmation for opt-out button and updated button classes`

--- components/montly-giving-card/index.tsx
+++ components/montly-giving-card/index.tsx
@@ -1,5 +1,8 @@
+"use client";
 import Image from "next/image";
 import GivingPic from "@/public/assets/images/7039bff9e79267b190fbc0db2afd3294c28b1ba4.jpg";
+import { useState } from "react";
+import Modal from "@/components/modal";
 
 interface Campaign {
   title: string;
@@ -11,6 +14,8 @@ export default function MonthlyGivingCard({
 }: {
   campaign: Campaign;
 }) {
+  const [modalClosed1, setModalClosed1] = useState(true);
+
   return (
     <div>
       <div className="md:grid md:grid-cols-12 flex flex-col items-stretch border border-gray-500 rounded-2xl md:h-[300px] md:pr-5 overflow-hidden gap-5 md:gap-5 w-full">
@@ -21,7 +26,7 @@ export default function MonthlyGivingCard({
               src={GivingPic}
               alt="giving pic"
               fill
-              className="object-cover rounded-t-2xl"
+              className="object-cover md:rounded-l-2xl rounded-l-none md:rounded-t-none rounded-t-2xl"
             />
           </div>
         </div>
@@ -40,9 +45,53 @@ export default function MonthlyGivingCard({
             Read more
           </button>
 
-          <button className="btn-secondary py-3.5 font md:px-0 px-4 text-white rounded-xl hover:scale-105 cursor-pointer transition">
+          <button
+            onClick={() => setModalClosed1(!modalClosed1)}
+            className="btn-secondary py-3.5 font md:px-0 px-4 text-white rounded-xl hover:scale-105 cursor-pointer transition"
+          >
             Opt - out
           </button>
+          {/* DELETE CONFIRMATION MODAL */}
+          <div>
+            <Modal
+              closeModal={() => setModalClosed1(!modalClosed1)}
+              isModalClosed={modalClosed1}
+              parentClassName="!py-6 md:!py-10  !items-start flex items-center justify-center"
+              className="!w-11/12 md:!w-fit"
+            >
+              <div className="max-w-md w-full rounded-2xl bg-white p-6 shadow-2xl md:p-8">
+                <div className="flex flex-col items-center text-center">
+                  <h1 className="text-[#00304C] text-2xl font-bold px-4">
+                    Are you sure you want to opt out from giving monthly to this
+                    cause
+                  </h1>
+
+                  {/* The visible border */}
+                  <div className="w-full py-6">
+                    <div className="h-[1px] w-full bg-gray-200" />
+                  </div>
+
+                  {/* Buttons - Made responsive so they don't overlap */}
+                  <div className="flex flex-row gap-3 w-full justify-center">
+                    <button
+                      type="button"
+                      className="btn-primary flex-1 py-3 text-white rounded-xl cursor-pointer transition-transform duration-300 hover:scale-105"
+                    >
+                      Yes, Delete
+                    </button>
+
+                    <button
+                      type="button"
+                      onClick={() => setModalClosed1(!modalClosed1)}
+                      className="btn-secondary flex-1 py-3 text-white rounded-xl cursor-pointer transition-transform duration-300 hover:scale-105"
+                    >
+                      No, Cancel
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </Modal>
+          </div>
         </div>
       </div>
     </div>
