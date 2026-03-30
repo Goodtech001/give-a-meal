cf6508b04dbdfe6acbdac9fadeef3da42cdb97f1
jehovah-son <chukwukasuccess0@gmail.com>
2026-03-30 12:18:25 +0100

    `Update about page layout with new components`

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,17 +1,18 @@
-import AboutDonate from '@/layout/about-donate'
-import AboutHero from '@/layout/about-hero'
-import Subscribe from '@/layout/subscribe'
-import TopNavBar from '@/layout/Topnavbar'
-import React from 'react'
+import AboutDonate from "@/layout/about-donate";
+import AboutHero from "@/layout/about-hero";
+import Subscribe from "@/layout/subscribe";
+import TopNavBar from "@/layout/Topnavbar";
+import ImageSlider from "@/section/who-are-we/image-slider";
 
 function About() {
   return (
     <div>
       <TopNavBar />
-      <AboutHero/>
-      <Subscribe/>
+      <AboutHero />
+      <ImageSlider />
+      <Subscribe />
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
