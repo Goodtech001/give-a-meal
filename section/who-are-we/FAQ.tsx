593f96c12662c67434fdea8a6d664f1e752a28af
jehovah-son <chukwukasuccess0@gmail.com>
2026-03-30 18:39:29 +0100

    `Updated FAQ component with minor styling changes`

--- section/who-are-we/FAQ.tsx
+++ section/who-are-we/FAQ.tsx
@@ -33,11 +33,13 @@ export default function FAQ() {
   const [activeId, setActiveId] = useState<number | null>(null);
 
   return (
-    <div className="container mx-auto justify-center flex  pt-20">
+    <div className="container mx-auto justify-center flex  pt-20 md:px-0 px-2">
       <div className="border border-gray-400 rounded-2xl w-full max-w-3xl overflow-hidden">
         {/* HEADER */}
         <div className="border-b border-gray-400 p-4 flex justify-center">
-          <p className="font-semibold text-text">Frequently Asked Questions</p>
+          <p className="font-semibold text-text font  text-lg">
+            Frequently Asked Questions
+          </p>
         </div>
 
         {/* FAQ LIST */}
@@ -51,17 +53,17 @@ export default function FAQ() {
             >
               {/* QUESTION */}
               <div
-                className="flex justify-between items-center py-4 px-6 cursor-pointer"
+                className="flex justify-between items-center py-4 px-6 md:gap-0 gap-3 cursor-pointer"
                 onClick={() => setActiveId(isOpen ? null : faq.id)}
               >
-                <p className="text-text font-medium">{faq.question}</p>
+                <p className="text-text font font-medium">{faq.question}</p>
 
                 <Icon
                   icon={isOpen ? "line-md:minus" : "ic:round-add"}
                   width="26"
                   height="26"
                   className={`transition-all duration-300 ${
-                    isOpen ? "text-[#0D7FC1]" : "text-gray-600"
+                    isOpen ? "text-text" : "text-gray-600"
                   }`}
                 />
               </div>
@@ -72,7 +74,7 @@ export default function FAQ() {
                   isOpen ? "max-h-40 py-4" : "max-h-0"
                 }`}
               >
-                <p className="text-sm text-gray-600 leading-relaxed">
+                <p className="text-sm text-text font leading-relaxed">
                   {faq.answer}
                 </p>
               </div>
