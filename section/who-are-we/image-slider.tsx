ebeb019599823aedcdaf2d18dbba5f6210c56a03
jehovah-son <chukwukasuccess0@gmail.com>
2026-03-30 13:11:53 +0100

    `Added image slider component with repeated image asset`

--- section/who-are-we/image-slider.tsx
+++ section/who-are-we/image-slider.tsx
@@ -1,3 +1,43 @@
+import WhoAreWeImage from "@/public/assets/images/whoareweImage.png";
+import Image from "next/image";
+
+const imageslide = [
+  {
+    img: WhoAreWeImage,
+  },
+  {
+    img: WhoAreWeImage,
+  },
+  {
+    img: WhoAreWeImage,
+  },
+  {
+    img: WhoAreWeImage,
+  },
+  {
+    img: WhoAreWeImage,
+  },
+  {
+    img: WhoAreWeImage,
+  },
+];
+
 export default function ImageSlider() {
-  return <div>ImageSlider</div>;
+  return (
+    <div>
+      <div className="container mx-auto pt-10">
+        <div className="md:gap-7 gap-3.5 flex overflow-x-scroll no-scrollbar whitespace-nowrap">
+          {imageslide.map((image, index) => (
+            <div key={index} className="flex-shrink-0 flex justify-center">
+              <Image
+                src={image.img}
+                alt="partner logo"
+                className="object-contain md:w-full w-74 h-64"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
