e901075107b907c82a13b039ea9bb6314ff6ea8a
jehovah-son <chukwukasuccess0@gmail.com>
2026-04-03 11:31:09 +0100

    `Refactor components: testimonial and mission layout`

--- components/testimonial/index.tsx
+++ components/testimonial/index.tsx
@@ -35,83 +35,86 @@ export default function TestimonialCard() {
   return (
     <div className="mt-15">
       {/* Title */}
-      <div className="text-center space-y-4">
-        <h1 className="font text-[#00304C] text-3xl font-semibold">The Power Of Giving</h1>
-      <p className="font text-[#00304CCC]">
-        Discover why thousands of donors choose Give A Meal <br /> to make a difference
-        every day.
-      </p>
+      <div className="text-center space-y-4 md:px-0 px-3 max-w-md mx-auto">
+        <h1 className="font text-[#00304C] text-3xl font-semibold">
+          The Power Of Giving
+        </h1>
+        <p className="font text-[#00304CCC] ">
+          Discover why thousands of donors choose Give A Meal to make a
+          difference every day.
+        </p>
       </div>
-
 
       <div className="relative p-8">
-        <div className="flex flex-col items-center justify-center py-10 px-4 mt-10 z-99 bg-white">
-        <div className="relative w-full max-w-2xl bg-white rounded-xl shadow-sm p-8 md:p-10 text-cente fle md:flex-col  md:items-center md:justify-between">
-          {/* Left Arrow */}
-          <button
-            onClick={prevSlide}
-            className="absolute -left-11 md:-left-20  top-1/2 -translate-y-1/2 bg-white shadow rounded-lg  p-2 hover:bg-gray-100 transition"
-          >
-            <ChevronLeftIcon className="h-5 w-5 text-gray-500" />
-          </button>
+        <div className="flex flex-col items-center justify-center py-10 px-4 z-99 bg-white">
+          <div className="relative w-full max-w-2xl bg-white rounded-xl shadow-sm p-8 md:p-10 text-cente fle md:flex-col  md:items-center md:justify-between">
+            {/* Left Arrow */}
+            <button
+              onClick={prevSlide}
+              className="absolute -left-11 md:-left-20  top-1/2 -translate-y-1/2 bg-white shadow rounded-lg  p-2 hover:bg-gray-100 transition"
+            >
+              <ChevronLeftIcon className="h-5 w-5 text-gray-500" />
+            </button>
 
-          {/* Content */}
-          {/* <div className="text-4xl text-gray-300 mb-4">“</div> */}
-          <Icon
-            icon="entypo:quote"
-            width="30"
-            height="30"
-            className="text-[#03324E66]"
-          />
-          <div className="md:flex mt-5">
-            <div className="md:pr-8">
-              <p className="text-gray-700 leading-relaxed mb-6 font">{text}</p>
+            {/* Content */}
+            {/* <div className="text-4xl text-gray-300 mb-4">“</div> */}
+            <Icon
+              icon="entypo:quote"
+              width="30"
+              height="30"
+              className="text-[#03324E66]"
+            />
+            <div className="md:flex mt-5">
+              <div className="md:pr-8">
+                <p className="text-gray-700 leading-relaxed mb-6 font">
+                  {text}
+                </p>
+              </div>
+
+              {/* Image Placeholder */}
+              <div className="mt-6 md:mt-0 shrink-0">
+                <div className="w-24 h-24 bg-gray-300 rounded-xl mx-auto md:mx-0"></div>
+              </div>
             </div>
 
-            {/* Image Placeholder */}
-            <div className="mt-6 md:mt-0 shrink-0">
-              <div className="w-24 h-24 bg-gray-300 rounded-xl mx-auto md:mx-0"></div>
-            </div>
+            <p className="font-semibold text-[#1e4356] font flex md:justify-end-safe">
+              {author}
+            </p>
+            {/* Right Arrow */}
+            <button
+              onClick={nextSlide}
+              className="absolute -right-11 md:-right-20 top-1/2 -translate-y-1/2 bg-white shadow rounded-lg p-2 hover:bg-gray-100 transition"
+            >
+              <ChevronRightIcon className="h-5 w-5 text-gray-500" />
+            </button>
           </div>
 
-          <p className="font-semibold text-[#1e4356] font flex md:justify-end-safe">
-            {author}
-          </p>
-          {/* Right Arrow */}
-          <button
-            onClick={nextSlide}
-            className="absolute -right-11 md:-right-20 top-1/2 -translate-y-1/2 bg-white shadow rounded-lg p-2 hover:bg-gray-100 transition"
-          >
-            <ChevronRightIcon className="h-5 w-5 text-gray-500" />
-          </button>
+          {/* Dots */}
+          <div className="flex justify-center space-x-2 mt-4">
+            {testimonials.map((_, index) => (
+              <button
+                key={index}
+                onClick={() => setCurrent(index)}
+                className={`w-3 h-3 rounded-full ${
+                  index === current ? "bg-[#1e4356]" : "bg-gray-300"
+                }`}
+              />
+            ))}
+          </div>
         </div>
 
-        {/* Dots */}
-        <div className="flex justify-center space-x-2 mt-4">
-          {testimonials.map((_, index) => (
-            <button
-              key={index}
-              onClick={() => setCurrent(index)}
-              className={`w-3 h-3 rounded-full ${
-                index === current ? "bg-[#1e4356]" : "bg-gray-300"
-              }`}
-            />
-          ))}
-        </div>
-      </div>
-
-      <motion.div
-        className="absolute top-0 left-40 w-60 h-60 bg-orange-300 rounded-full blur-3xl opacity-20"
-        initial={{ scale: 0 }}
-        animate={{ scale: 1 }}
-        transition={{ duration: 1, ease: "easeInOut" }}
-      ></motion.div>
-      <motion.div
-        className="absolute -bottom-20 right-40 w-60 h-60 bg-orange-300 rounded-full blur-2xl opacity-10"
-        initial={{ scale: 0 }}
-        animate={{ scale: 1 }}
-        transition={{ duration: 1, ease: "easeInOut", delay: 0.2 }}
-      ></motion.div>
+        <motion.div
+          className="absolute top-0 left-40 w-60 h-60 bg-orange-300 rounded-full blur-3xl opacity-20"
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+        ></motion.div>
+        <motion.div
+          className="absolute -bottom-20 right-40 w-60 h-60 bg-orange-300 rounded-full blur-2xl opacity-10"
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 1, ease: "easeInOut", delay: 0.2 }}
+        ></motion.div>
       </div>
     </div>
   );
--- layout/Join-mission/index.tsx
+++ layout/Join-mission/index.tsx
@@ -1,66 +1,71 @@
-import Image from 'next/image';
-import React, { useRef } from 'react';
+import Image from "next/image";
+import React, { useRef } from "react";
 import mission from "@/public/assets/images/7039bff9e79267b190fbc0db2afd3294c28b1ba4.jpg";
-import { Icon } from '@iconify/react';
-import { useInView, motion } from 'framer-motion';
+import { Icon } from "@iconify/react";
+import { useInView, motion } from "framer-motion";
 
 function Mission() {
   const ref = useRef(null);
-  const isInView = useInView(ref, { once: true, margin: '-50px' });
+  const isInView = useInView(ref, { once: true, margin: "-50px" });
 
   return (
     <motion.div
       ref={ref}
       initial={{ opacity: 0, y: 40 }}
       animate={isInView ? { opacity: 1, y: 0 } : {}}
-      transition={{ duration: 0.8, ease: 'easeOut' }}
+      transition={{ duration: 0.8, ease: "easeOut" }}
       className="relative mt-10"
     >
-      <h1 className='font text-4xl flex items-center justify-center font-semibold text-[#00304C]'>
-        Join the Mission
-      </h1>
-      <p className='flex items-center justify-center text-center font mt-6 text-[#00304CCC]'>
-        Find a story that moves you and contribute to our ongoing goals. With Give A Meal, <br />
-        your generosity goes exactly where it’s needed most.
-      </p>
+      <div className="container mx-auto  md:px-0 px-3">
+        {" "}
+        <h1 className=" font text-4xl flex items-center justify-center font-semibold text-[#00304C]">
+          Join the Mission
+        </h1>
+        <p className="flex items-center justify-center text-center font mt-6 text-[#00304CCC]">
+          Find a story that moves you and contribute to our ongoing goals. With
+          Give A Meal, <br />
+          your generosity goes exactly where it’s needed most.
+        </p>
+      </div>
 
-      <motion.div 
-        whileHover={{ scale: 1.05 }} 
-        className='border-none md:border md:gap- flex md:flex-row flex-col items-center justify-center mt-15 px-6'
+      <motion.div
+        whileHover={{ scale: 1.05 }}
+        className="border-none md:border md:gap- flex md:flex-row flex-col items-center justify-center mt-15 px-6"
       >
-        <div className='relative'>
-          <Image 
-            src={mission} 
-            alt='' 
-            className='md:w-100 w-full md:rounded-l-2xl rounded-2xl brightness-50' 
+        <div className="relative">
+          <Image
+            src={mission}
+            alt=""
+            className="md:w-100 w-full md:rounded-l-2xl rounded-2xl brightness-50"
           />
-          <div className='border bg-[#FF8F07] text-white flex w-fit font items-center p-4 rounded-tl-2xl rounded-br-2xl gap-2 absolute top-0 border-transparent'>
+          <div className="border bg-[#FF8F07] text-white flex w-fit font items-center p-4 rounded-tl-2xl rounded-br-2xl gap-2 absolute top-0 border-transparent">
             <Icon icon="boxicons:siren" width="24" height="24" />
             <p>SPOTLIGHT CAUSE</p>
           </div>
         </div>
 
-        <motion.div 
-          whileHover={{ scale: 1.05 }} 
-          className='md:border border-gray-300 py-8 md:px-15 px-25 bg-white md:rounded-r-2xl shadow-lg w-fit'
+        <motion.div
+          whileHover={{ scale: 1.05 }}
+          className="md:border border-gray-300 py-8 md:px-15 px-25 bg-white md:rounded-r-2xl shadow-lg w-fit"
         >
-          <h1 className='text-[#00304C] font md:text-3xl text-xl truncate font-semibold'>
+          <h1 className="text-[#00304C] font md:text-3xl text-xl truncate font-semibold">
             Help reach hungry families <br /> in the north
           </h1>
-          <p className='font mt-6 text-[#00304CCC] truncate'>
-            Shared meals will provide hot meals, monthly <br /> food baskets and nutrition supplements to <br /> families in Northern Nigeria...
+          <p className="font mt-6 text-[#00304CCC] truncate">
+            Shared meals will provide hot meals, monthly <br /> food baskets and
+            nutrition supplements to <br /> families in Northern Nigeria...
           </p>
 
-          <div className='space-x-4 mt-6'>
-            <motion.button 
-              whileHover={{ scale: 1.1 }} 
-              className='border py-2 px-4 rounded-xl border-transparent text-white font bg-[#0D7FC1] transition duration-300'
+          <div className="space-x-4 mt-6">
+            <motion.button
+              whileHover={{ scale: 1.1 }}
+              className="border py-2 px-4 rounded-xl border-transparent text-white font bg-[#0D7FC1] transition duration-300"
             >
               Read More
             </motion.button>
-            <motion.button 
-              whileHover={{ scale: 1.1 }} 
-              className='border py-2 px-4 rounded-xl border-transparent text-white font bg-[#FF8F07] transition duration-300'
+            <motion.button
+              whileHover={{ scale: 1.1 }}
+              className="border py-2 px-4 rounded-xl border-transparent text-white font bg-[#FF8F07] transition duration-300"
             >
               Donate
             </motion.button>
@@ -68,17 +73,17 @@ function Mission() {
         </motion.div>
       </motion.div>
 
-      <motion.div 
+      <motion.div
         className="absolute top-10 left-20 w-40 h-40 bg-blue-600 rounded-full blur-3xl opacity-20"
         initial={{ scale: 0 }}
         animate={{ scale: 1 }}
-        transition={{ duration: 1, ease: 'easeInOut' }}
+        transition={{ duration: 1, ease: "easeInOut" }}
       ></motion.div>
-      <motion.div 
+      <motion.div
         className="absolute -bottom-20 right-20 w-40 h-40 bg-blue-600 rounded-full blur-2xl opacity-10"
         initial={{ scale: 0 }}
         animate={{ scale: 1 }}
-        transition={{ duration: 1, ease: 'easeInOut', delay: 0.2 }}
+        transition={{ duration: 1, ease: "easeInOut", delay: 0.2 }}
       ></motion.div>
     </motion.div>
   );
